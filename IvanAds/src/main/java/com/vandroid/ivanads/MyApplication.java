00b3c198b7ad9fc68e64ad41c8ead74c97461731
Emas Hati Group <akun1khusus@gmail.com>
2022-02-08 19:56:25 +0700

    Adding One Signal

--- IvanAds/src/main/java/com/vandroid/ivanads/MyApplication.java
+++ IvanAds/src/main/java/com/vandroid/ivanads/MyApplication.java
@@ -8,10 +8,11 @@ import com.flurry.android.FlurryPerformance;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.onesignal.OneSignal;
 
 public class MyApplication extends Application {
     private static IvanOpenAds ivanOpenAds;
-
+    private static final String ONESIGNAL_APP_ID = "1317aafc-af05-49c2-940b-1b8b2ce587fc";
     @Override
     public void onCreate() {
         super.onCreate();
@@ -29,9 +30,13 @@ public class MyApplication extends Application {
                 .withIncludeBackgroundSessionsInMetrics(true)
                 .withLogLevel(Log.VERBOSE)
                 .withPerformanceMetrics(FlurryPerformance.ALL)
-                .build(this, "9CFC3HDGJYSFTRKJG98N");
+                .build(this, "Your_Id_Flurry");
         ivanOpenAds = new IvanOpenAds(this);
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
 
-
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
     }
 }
